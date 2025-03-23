76459a164d2a377719b315d8dddd8c37afa2a60e
MineHayato <ht23a102@oecu.jp>
2025-03-23 07:07:42 +0000

    不要な代入を避ける記法

--- 03-fp/03-first-class/greeter.js
+++ 03-fp/03-first-class/greeter.js
@@ -8,3 +8,18 @@ function greeter(target) {
 
 const greet = greeter('Step Jun');
 greet();
+
+// 簡略化
+function greeter2(target) {
+    return () => {
+        console.log(`Hi, ${target}!`);
+    };
+}
+
+const greet2 = greeter2('A');
+greet2();
+
+// 簡略化2
+const greeter3 = (target) => () => { console.log(`Hi, ${target}!`); };
+const greet3 = greeter2('B');
+greet3();
