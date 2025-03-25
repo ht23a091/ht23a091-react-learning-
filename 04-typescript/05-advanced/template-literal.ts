26882c2cc6be9593abcf808569a3a2051209ffd6
MineHayato <ht23a102@oecu.jp>
2025-03-25 10:00:27 +0000

    テンプレートリテラル型で文字列を型に

---
+++ 04-typescript/05-advanced/template-literal.ts
@@ -0,0 +1,16 @@
+type DateFormat = `${number}-${number}-${number}`;
+
+const date1: DateFormat = '2022-09-01';
+// const date2: DateForMat = 'Sep. 1, 2022';
+
+interface MonthMap {
+    [Key: `month_${number}`]: string;
+}
+
+const monthMap: MonthMap = {
+    month_01: 'January',
+    month_02: 'Februaly',
+};
+
+monthMap.month_03 = 'March';
+// onthMap.four = 'April';
