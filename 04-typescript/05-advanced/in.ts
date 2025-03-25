0b8f5256d27185c8274781a0433efb8d33ed969b
MineHayato <ht23a102@oecu.jp>
2025-03-25 08:37:07 +0000

    in演算子でキーの値を限定

---
+++ 04-typescript/05-advanced/in.ts
@@ -0,0 +1,13 @@
+const obj = { a: 1, b: 2, c: 3};
+console.log('a' in obj);
+for (const key in obj) { console.log(key); };
+
+type Fig = 'one' | 'two' | 'three';
+type FigMap = { [Key in Fig]?: number };
+
+const figMap: FigMap = {
+    one: 1,
+    two: 2,
+    three: 3,
+};
+// figMap.four = 4;
