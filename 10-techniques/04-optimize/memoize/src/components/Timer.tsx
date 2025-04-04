cbb041f227a7c4ed702ea1bc884ffff6ebcbbd96
MineHayato <ht23a102@oecu.jp>
2025-04-04 10:32:56 +0000

    ファイル名変更

--- 10-techniques/04-optimize/memoize/src/components/Timer.tsx
+++ 10-techniques/04-optimize/memoize/src/components/Timer.tsx
@@ -1,7 +1,7 @@
 import type {FC} from 'react';
 import { RepeatClockIcon as ResetIcon} from '@chakra-ui/icons';
 import { Box, Button, Stat, StatLabel, StatNumber } from '@chakra-ui/react';
-import { useTimer } from 'hooks/useTime_memo';
+import { useTimer } from 'hooks/useTimer_memo';
 
 type Props = { maxCount?: number };
 const MAX_COUNT = 60;
