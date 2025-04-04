6f50cec5498401e4ae30a00aedf24ffd8c9645e0
MineHayato <ht23a102@oecu.jp>
2025-04-04 10:48:39 +0000

    useRefを使ってタイマーを作成

--- 10-techniques/04-optimize/memoize/src/components/Timer.tsx
+++ 10-techniques/04-optimize/memoize/src/components/Timer.tsx
@@ -1,7 +1,7 @@
 import type {FC} from 'react';
 import { RepeatClockIcon as ResetIcon} from '@chakra-ui/icons';
 import { Box, Button, Stat, StatLabel, StatNumber } from '@chakra-ui/react';
-import { useTimer } from 'hooks/useTimer_memo';
+import { useTimer } from 'hooks/useTimer_useRef';
 
 type Props = { maxCount?: number };
 const MAX_COUNT = 60;
